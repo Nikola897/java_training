999f18f54b7117314ce46a043056ab8fc2469b32
Nikola897 <bsc.nikola.stojanovic@gmail.com>
2022-07-04 16:25:51 +0200

    initial add to repo

---
+++ src/basicks/HelloWorld.java
@@ -0,0 +1,34 @@
+package basicks;
+
+public class HelloWorld {
+	
+	public static void main (String[] arg) {
+		
+		//Create 
+		
+		String month = "February";
+		 
+		 switch (month) {
+		 case "January" :
+		 System.out.println("Today is January");
+		 break;
+		 case "February" :
+		 System.out.println("Today is February");
+		 break;
+		 case "March" :
+		 System.out.println("Today is March");
+		 break;
+		 case "April" :
+		 System.out.println("Today is April");
+		 break;
+		 case "May" :
+		 System.out.println("Today is May");
+		 break;
+		 default : System.out.println("Error!");
+		 break;
+		}
+		
+		
+	}
+
+}
